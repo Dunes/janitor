01ad9a02409bbdafd58832fd05f64f006968fbfe
Jack Hargreaves <jxh576@cs.bham.ac.uk>
2014-06-17 14:01:30 +0100

    added usage print out for run_problems.sh

--- simulator/run_problems.sh
+++ simulator/run_problems.sh
@@ -1,14 +1,37 @@
 #! /bin/bash
 
+
+function usage {
+	echo "Usage: run_problems.sh [OPTION]... FILE"
+	echo "Run the problems listed in FILE (one per line)."
+	echo "Records output of problems into a log directory. One file per problem."
+	echo "Separately records all plans produced by a problem in sub dir of the logging" 
+	echo "  directory called \`plans'"
+	echo "Record problems where a solution was not found (failed runs) to a log file."
+	echo "When FILE is -, read standard input."
+	echo ""
+	echo "  -t TIME            Run problems with a planning time of TIME. (default 30)"
+	echo "  -e ERROR_OUTPUT    The file to record failed problems to. "
+	echo "                       (default \`unsolved-problems.txt')"
+	echo "  -l LOG_DIR         The logging directory to log output and failed runs to."
+	echo "                       (default \`logs')"
+	echo "  -h                 Print this usage instructions."
+
+	exit 1
+}
+
 # defaults
 time_opt="30"
-wait_opt=""
 log_dir="logs"
 error_log_file="unsolved-problems.txt"
 
-args=`getopt Wwt:e:l: $*`
+args=`getopt -q t:e:l: $*`
+if [[ $? -ne 0 ]]
+then
+	usage
+fi
 set -- $args
-while [ $1 ]
+while [[ $1 ]]
 do
 	case "$1" in
 	-t)
@@ -23,22 +46,22 @@ do
 		shift
 		log_dir="$1"
 		;;
-	-[Ww])
-		wait_opt="$1"
-		;;
 	--)
 		shift
 		break
 		;;
 	*)
-		echo "unrecognised option '$1'"
-		exit 1
+		echo "unrecognised option \`$1'"
+		usage
 	esac
 	shift
 done
 
 input="$1"
-if [ "$input" == "-" ]
+if [[ ! "$input" ]]
+then
+	usage
+elif [[ "$input" == "-" ]]
 then
 	input="/dev/stdin"
 fi
@@ -48,7 +71,7 @@ error_log="$log_dir/$error_log_file"
 while read file_name
 do
 	echo "starting $file_name"
-	./simulator.py "$file_name" -t "$time_opt" "$wait_opt" -l "$log_dir"
+	./simulator.py "$file_name" -t "$time_opt" -l "$log_dir"
 	exit_val="$?"
 	if [ "$exit_val" -ne 0 ]
 	then
