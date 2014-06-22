c0c03a367a0796e4cca673fc17da150c60715dc8
Jack Hargreaves <jxh576@cs.bham.ac.uk>
2014-06-22 13:34:42 +0100

    logger unittests

--- simulator/logger.py
+++ simulator/logger.py
@@ -1,6 +1,5 @@
 from os.path import join, basename, splitext, isdir
 from os import makedirs
-from errno import EEXIST
 
 class Logger(object):
 
@@ -12,23 +11,23 @@ class Logger(object):
 	def get_plan_log_file_name(cls, log_file_name):
 		name, ext = splitext(log_file_name)
 		return name + "-plans" + ext
-	
+
 	def __init__(self, log_file_name, working_directory="./logs", plans_subdir="plans"):
 		self._create_if_not_exists(working_directory)
 		self._create_if_not_exists(join(working_directory, plans_subdir))
 		self.log_file_name = join(working_directory, log_file_name)
 		self.plan_log_file_name = join(working_directory, plans_subdir, self.get_plan_log_file_name(log_file_name))
-		self.log = None 
+		self.log = None
 		self.plan_log = None
-	
+
 	def _create_if_not_exists(self, path):
 		try:
-		    makedirs(path)
-		except OSError as exc: # Python >2.5
-		    if exc.errno == EEXIST and isdir(path):
-		        pass
-		    else: raise
-	
+			makedirs(path)
+		except FileExistsError: # Python >2.5
+			if isdir(path):
+				pass
+			else: raise
+
 	def log_property(self, name, value, stringify=str):
 		if not self.log:
 			self.log = open(self.log_file_name, "w")
@@ -37,28 +36,28 @@ class Logger(object):
 		self.log.write(": ")
 		self.log.write(stringify(value))
 		self.log.write(",\n")
-	
+
 	def log_plan(self, plan):
 		if not self.plan_log:
 			self.plan_log = open(self.plan_log_file_name, "w")
 		self.plan_log.write(repr(plan))
 		self.plan_log.write("\n")
-		
+
 	def close(self):
-		e = None
+		error = None
 		try:
 			if self.log and not self.log.closed:
 				self.log.write("}\n")
 				self.log.close()
 		except IOError as e:
-			pass
+			error = e
 		if self.plan_log and not self.plan_log.closed:
 			self.plan_log.close()
-		if e:
-			raise e
-	
-	def __exit__(self, type, value, tb):
+		if error:
+			raise error
+
+	def __exit__(self, type_, value, tb):
 		self.close()
-	
+
 	def __enter__(self):
 		return self
---
+++ simulator/test/test_logger.py
@@ -0,0 +1,169 @@
+'''
+Created on 21 Jun 2014
+
+@author: jack
+'''
+import unittest
+from unittest.mock import patch, Mock, DEFAULT, call
+
+from logger import Logger
+
+from io import StringIO
+
+class LoggerTest(unittest.TestCase):
+
+
+    def setUp(self):
+        self.patch = patch("logger.Logger.__init__", return_value=None)
+        self.patch.start()
+
+
+    def tearDown(self):
+        self.patch.stop()
+
+    def test_get_log_file_name(self):
+        expected = "problem_name-planning_time(0).log"
+        actual = Logger.get_log_file_name("problem_name", 0)
+        self.assertEqual(expected, actual)
+
+    def test_get_plan_log_file_name(self):
+        expected = "log_file_name-plans.log"
+        actual = Logger.get_plan_log_file_name("log_file_name.log")
+        self.assertEqual(expected, actual)
+
+    @patch("logger.makedirs")
+    def test_create_if_not_exists_not_exist(self, mocked_makedirs):
+        path = "path"
+        Logger("log_file_name")._create_if_not_exists(path)
+        mocked_makedirs.assert_called_once_with(path)
+
+    @patch.multiple("logger", makedirs=DEFAULT, isdir=DEFAULT)
+    def test_create_if_not_exists_and_exists_as_dir(self, makedirs, isdir):
+        path = "path"
+        makedirs.side_effect = FileExistsError
+        isdir.return_value = True
+
+        Logger("log_file_name")._create_if_not_exists(path)
+
+        makedirs.assert_called_once_with(path)
+        isdir.assert_called_once_with(path)
+
+    @patch.multiple("logger", makedirs=DEFAULT, isdir=DEFAULT)
+    def test_create_if_not_exists_and_exists_as_not_dir(self, makedirs, isdir):
+        path = "path"
+        makedirs.side_effect = FileExistsError
+        isdir.return_value = False
+
+        self.assertRaises(FileExistsError, Logger("log_file_name")._create_if_not_exists, path)
+
+        makedirs.assert_called_once_with(path)
+        isdir.assert_called_once_with(path)
+
+    def test_log_property(self):
+        logger = Logger()
+        logger.log = StringIO()
+
+        logger.log_property("name", "value")
+
+        self.assertEqual("'name': value,\n", logger.log.getvalue())
+
+    def test_log_property_with_repr(self):
+        logger = Logger()
+        logger.log = StringIO()
+
+        logger.log_property("name", "value", stringify=repr)
+
+        self.assertEqual("'name': 'value',\n", logger.log.getvalue())
+
+    @patch("logger.open", return_value=StringIO(), create=True)
+    def test_log_property_log_not_open_yet(self, thing):
+        logger = Logger()
+        logger.log_file_name = None
+        logger.log = None
+
+        logger.log_property("name", "value")
+
+        self.assertEqual("{\n'name': value,\n", logger.log.getvalue())
+
+    def test_log_plan(self):
+        logger = Logger()
+        logger.plan_log = StringIO()
+
+        logger.log_plan(["plan"])
+
+        self.assertEqual("['plan']\n", logger.plan_log.getvalue())
+
+    @patch("logger.open", return_value=StringIO(), create=True)
+    def test_log_plan_log_not_open_yet(self, thing):
+        logger = Logger()
+        logger.plan_log_file_name = None
+        logger.plan_log = None
+
+        logger.log_plan(["plan"])
+
+        self.assertEqual("['plan']\n", logger.plan_log.getvalue())
+
+    def test_close(self):
+        logger = Logger()
+        logger.log = Mock()
+        logger.log.closed = False
+        logger.plan_log = Mock()
+        logger.plan_log.closed = False
+
+        logger.close()
+
+        logger.log.write.assert_called_once_with("}\n")
+        logger.log.close.assert_called_once_with()
+        logger.plan_log.close.assert_called_once_with()
+
+    def test_close_when_already_closed(self):
+        logger = Logger()
+        logger.log = Mock()
+        logger.log.closed = True
+        logger.plan_log = Mock()
+        logger.plan_log.closed = True
+
+        logger.close()
+
+        self.assertEqual(False, logger.log.write.called)
+        self.assertEqual(False, logger.log.close.called)
+        self.assertEqual(False, logger.plan_log.close.called)
+
+    def test_close_reraise_ioerror(self):
+        logger = Logger()
+        logger.log = Mock()
+        logger.log.closed = False
+        logger.log.write.side_effect = IOError
+        logger.plan_log = Mock()
+        logger.plan_log.closed = False
+
+        self.assertRaises(IOError, logger.close)
+
+        logger.log.write.assert_called_once_with("}\n")
+        self.assertEqual(False, logger.log.close.called)
+        logger.plan_log.close.assert_called_once_with()
+
+class LoggerInitTest(unittest.TestCase):
+
+    @patch.multiple("logger.Logger", _create_if_not_exists=DEFAULT, get_plan_log_file_name=DEFAULT)
+    def test_init(self, _create_if_not_exists, get_plan_log_file_name):
+        working_directory = "working_directory"
+        plans_subdir = "plans_subdir"
+        log_file_name = "log_file_name"
+        get_plan_log_file_name.return_value = log_file_name
+
+        log = Logger(log_file_name, working_directory, plans_subdir)
+
+        self.assertEqual(2, _create_if_not_exists.call_count)
+        _create_if_not_exists.assert_has_calls([
+            call(working_directory),
+            call("working_directory/plans_subdir"),
+        ])
+        self.assertEqual("working_directory/log_file_name", log.log_file_name)
+        self.assertEqual("working_directory/plans_subdir/log_file_name", log.plan_log_file_name)
+        self.assertEqual(None, log.log)
+        self.assertEqual(None, log.plan_log)
+
+
+if __name__ == "__main__":
+    unittest.main()
