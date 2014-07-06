41ac2b5dfa0097dfe300606723d109513785cb93
Jack Hargreaves <jxh576@cs.bham.ac.uk>
2014-07-06 13:45:34 +0100

    modified rounding error with milliseconds as floats in javascript

--- visualiser/chart.js
+++ visualiser/chart.js
@@ -1,12 +1,13 @@
-function createChart() {
-	var data = parseData();
+
+function createChart($, google) {
+	var data = parseData($);
 	var actions = data[0];
 	var colours = data[1];
-	drawChart(actions, colours);
+	drawChart(actions, colours, google);
 }
 
-function parseData() {
-	
+function parseData($) {
+
 	var chart_colours = {
 		"Move": "#8B0000",
 		"Clean": "#FFA500",
@@ -17,14 +18,14 @@ function parseData() {
 		"Partial ExtraClean": "#4d934d",
 		"Partial Move": "#ae4d4d"
 	};
-	
+
 	var data = $("#input-data").val().trim();
-	
+
 	data = data.replace(/[\[\]]/g, "");
 	data = data.split(/\), ?|\)/);
 	var actions = [];
 	var partial = [];
-	
+
 	var colours = {};
 	for (var index in data) {
 		var action = data[index];
@@ -35,11 +36,11 @@ function parseData() {
 		var result = createChartElementData(components, chart_colours, colours);
 		actions.push(result);
 	}
-	
+
 	actions.sort(function(x,y) {
 		if (x[0] === y[0]) {
 			return x[2] - y[2];
-		} else if (x[0] === "planner" || y[0] == undefined) {
+		} else if (x[0] === "planner" || y[0] === undefined) {
 			return -1;
 		} else if (x[0] === undefined || y[0] === "planner") {
 			return 1;
@@ -47,10 +48,10 @@ function parseData() {
 			return x[0].localeCompare(y[0]);
 		}
 	});
-	
+
 	var chartColourList = [];
 	var labels = {};
-	
+
 	for (var i in actions) {
 		var action = actions[i];
 		var label = action[1];
@@ -58,8 +59,8 @@ function parseData() {
 			chartColourList.push(colours[label]);
 			labels[label] = true;
 		}
-	}	
-	
+	}
+
 	return [actions, chartColourList];
 }
 
@@ -74,10 +75,10 @@ function createChartElementData(components, chart_colours, colours) {
 	}
 
 	var result = [
-		dict["agent"],
-		actionLabel(components[0], dict, chart_colours, colours, Boolean(dict["partial"])), // action name
-		parseFloat(dict["start_time"]) * 1000,
-		(parseFloat(dict["start_time"]) + parseFloat(dict["duration"])) * 1000
+		dict.agent,
+		actionLabel(components[0], dict, chart_colours, colours, Boolean(dict.partial)), // action name
+		secondsToMillis(dict.start_time),
+		secondsToMillis(dict.start_time) + secondsToMillis(dict.duration)
 	];
 	return result;
 }
@@ -86,13 +87,13 @@ function createChartElementData(components, chart_colours, colours) {
 
 function actionLabel(name, components, chart_colours, colours, partial) {
 	var label = name;
-	
+
 	if (name === "Move") {
-		label = "Move "+components["end_node"];
-	} else if (name === "Clean" || name == "ExtraClean") {
-		label = name + " " + components["room"];
+		label = "Move "+components.end_node;
+	} else if (name === "Clean" || name === "ExtraClean") {
+		label = name + " " + components.room;
 	}
-	
+
 	if (partial) {
 		name = "Partial " + name;
 		label = "Partial " + label;
@@ -100,14 +101,14 @@ function actionLabel(name, components, chart_colours, colours, partial) {
 
 	var colour = chart_colours[name];
 	if (!colour) {
-		colour = chart_colours["Other"];
+		colour = chart_colours.Other;
 	}
 	colours[label] = colour;
 
 	return label;
 }
 
-function drawChart(data, colours) {
+function drawChart(data, colours, google) {
 
   var container = document.getElementById('example3.1');
   var chart = new google.visualization.Timeline(container);
@@ -118,10 +119,16 @@ function drawChart(data, colours) {
   dataTable.addColumn({ type: 'number', id: 'Start' });
   dataTable.addColumn({ type: 'number', id: 'End' });
   dataTable.addRows(data);
-  
-  var options = { 
-  	"colors": colours
+
+  var options = {
+    "colors": colours
   };
-  
+
   chart.draw(dataTable, options);
+}
+
+function secondsToMillis(value) {
+    var seconds = parseFloat(value);
+    var millis = Math.round(seconds * 1000);
+    return millis;
 }
