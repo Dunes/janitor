61a279fd231def13e560683e62ed49e8262c12b1
Jack Hargreaves <jxh576@cs.bham.ac.uk>
2014-07-22 15:54:23 +0100

    added test runner to run all tests from liclipse

---
+++ simulator/test/all.py
@@ -0,0 +1,13 @@
+'''
+Created on 22 Jul 2014
+
+@author: jack
+'''
+
+import unittest
+
+def load_tests(loader, tests, pattern):
+    return loader.discover('.')
+
+if __name__ == '__main__':
+    unittest.main()
