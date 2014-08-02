289e844386d47746c8d2c67c65c299a275459611
Jack Hargreaves <jxh576@cs.bham.ac.uk>
2014-08-02 13:24:47 +0100

    removed round_half_up as no longer used

--- simulator/src/accuracy.py
+++ simulator/src/accuracy.py
@@ -1,6 +1,6 @@
 from decimal import Decimal, ROUND_DOWN, ROUND_CEILING
 
-__all__ = ["increment", "quantize", "round_half_up", "as_end_time", "as_start_time", "to_prev_start_time"]
+__all__ = ["increment", "quantize", "as_end_time", "as_start_time", "to_prev_start_time"]
 
 one = Decimal("1")
 
@@ -15,10 +15,6 @@ def quantize(value) -> Decimal:
 
 def to_prev_start_time(value: Decimal) -> Decimal:
     return as_start_time(value) - one
-
-
-def round_half_up(value: Decimal) -> Decimal:
-    return as_start_time(value)
 
 
 def as_start_time(value: Decimal) -> Decimal:
