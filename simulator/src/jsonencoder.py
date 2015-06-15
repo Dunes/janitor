7b75bb924c6193a43be83aa8d8024381a56b9d4b
Jack Hargreaves <jxh576@cs.bham.ac.uk>
2015-06-15 14:27:49 +0100

    json encoder for actions

--- simulator/src/jsonencoder.py
+++ simulator/src/jsonencoder.py
@@ -1,1 +1,19 @@
 __author__ = 'jack'
+
+from simplejson import JSONEncoder, dumps
+from action import Action
+from functools import partial
+
+__all__ = ["json_dumps", "ActionEncoder"]
+
+
+class ActionEncoder(JSONEncoder):
+    def default(self, o):
+        if isinstance(o, Action):
+            d = dict(type=type(o).__name__, **vars(o))
+            d.pop("apply", None)  # remove monkey patched partial apply if it exists
+            return d
+        return super().default(o)
+
+
+json_dumps = partial(dumps, cls=ActionEncoder)
