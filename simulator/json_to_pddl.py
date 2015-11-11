c06c0a0bb4970eefa134c4d52a2f8f38f917628d
Jack Hargreaves <jxh576@cs.bham.ac.uk>
2015-11-11 15:25:11 +0000

    converts json problem files into pddl files for direct use with optic

---
+++ simulator/json_to_pddl.py
@@ -0,0 +1,38 @@
+#! /usr/bin/python3
+
+import sys
+import pathlib
+
+src = pathlib.Path("./src").resolve()
+if not any(pathlib.Path(p).resolve() == src for p in sys.path):
+    sys.path.append(str(src))
+
+import argparse
+from roborescue.problem_encoder import encode_problem_to_file
+from problem_parser import decode
+
+
+def parser():
+    p = argparse.ArgumentParser(description="Turns json problems into pddl files")
+    p.add_argument("--model", "-m")
+    p.add_argument("--out", "-o")
+    return p
+
+
+def main(model, out):
+    model = decode(model)
+    encode_problem_to_file(
+        filename=out,
+        model=model,
+        agent="all",
+        goals=model["goal"],
+        metric=model["metric"],
+        time=0,
+        events=model["events"]
+    )
+
+
+if __name__ == "__main__":
+    args = parser().parse_args()
+    main(args.model, args.out)
+
